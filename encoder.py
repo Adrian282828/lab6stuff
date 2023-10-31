7922cc6e5f719ceb24b1662edf5557b2c4693562
NotQueso <88800044+NotQueso@users.noreply.github.com>
2023-10-31 19:19:18 -0400

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -1,44 +1,62 @@
-def encode(string):
-    numbers=[]
-    encodednum=''
-    for i in range(len(string)):
-        numbers.append(int(string[i])+3)
-    for j in range(len(numbers)):
-        encodednum+=str(numbers[j])
-        return encodednum
+def encode(password):
+    # Creates the empty encoded password
+    encoded_pass = ''
+
+    # Takes the un-coded password from the function parameter and forces it to be a string no matter the input
+    string_password = str(password)
+
+    for element in string_password:
+        # force each string number of the password to be an integer type.
+        # Then use the addition and modulo to implement the shift and encode operations respectively.
+        temp = str((int(element) + 3) % 9)
+
+        # tack on the coded numbers to the string
+        encoded_pass += temp
+
+    return encoded_pass
+
 
 def menu():
     print("Menu")
     print("-------------")
     print("1. Encode")
     print("2. Decode")
-    print("3. Quit")
-          
-def decode(encoded):
-    numbers=[]
-    newdecoded=''
-    for i in range(len(encoded)):
-        numbers.append(int(encoded[i])-3)
-    for j in range(len(numbers)):
-        newdecoded+=str(numbers[j])
-    return newdecoded 
+    print("3. Quit\n")
 
+def decode(encoded_password):
+    # Creates the empty decoded password
+    decoded_password = ''
+
+    for element in encoded_password:
+        # force each string number of the encoded password to be an integer type.
+        # Then use the subtraction and modulo to implement the shift and decode operations respectively.
+        temp = str((int(element) - 3) % 10)
+
+        # tack on the decoded numbers to the string
+        decoded_password += temp
+
+    return decoded_password
 
 
 def main():
-   z=0
-   while z==0:
+    while True:
         menu()
-        choice= input("Please enter an option: ")
+        choice = input("Please enter an option: ")
+
         if choice == "1":
-            code= input("Please enter your password to encode")
-            x= encode(code)
-            print("Your password has been encoded and stored!")
-        elif choice =="2":
-            print("the encoded password is",x,  "and the original password is" , code)
+            code = input("Please enter your password to encode: ")
+            x = encode(code)
+            print("Your password has been encoded and stored!\n")
+
+        elif choice == "2":
+            print("the encoded password is", x, "and the original password is", code)
+
         elif choice == "3":
-            z==1000
+            exit()
+
         else:
-            print("Wrong input")
+            print("Wrong input. Please try again!")
+
+
 if __name__ == '__main__':
     main()
